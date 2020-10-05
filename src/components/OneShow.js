8404b812c1a16ffe516ca0694c0d5baac04ab458
Tamer Hamoud <67854476+TamerHa-coder@users.noreply.github.com>
2020-10-05 09:56:03 +0300

    setup

---
+++ src/components/OneShow.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { Link, useParams } from 'react-router-dom';
+import './OneShow.css';
+import likedImg from "../media/liked.png"
+import notLikedImg from "../media/notLiked.png"
+
+function OneShow() {
+  const { id } = useParams(); //this is the selected show id
+
+    /*
+      
+      insert your code here 
+      
+      
+    */
+
+  return (
+    <div className='one-show-container'>
+      <Link className='go-back-link' to='/'>
+        <img
+          className='go-back-img'
+          alt='Go back'
+          src='https://img.icons8.com/metro/52/000000/circled-left-2.png'
+        />
+      </Link>
+      {/*
+      
+        insert your code here 
+      
+      
+      */}
+    </div>
+  );
+}
+
+export default OneShow;
